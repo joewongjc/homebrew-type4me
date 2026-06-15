4dc54026f3f423a48ddf3312c533e088e42ba47e
Jonathan Wong <joewong.wjc@gmail.com>
2026-06-15 15:01:10 -0700

    bump: Type4Me v1.9.6

--- Casks/type4me.rb
+++ Casks/type4me.rb
@@ -1,6 +1,6 @@
 cask "type4me" do
   version "1.9.6"
-  sha256 "a963fa5f563a30890b6133c997487115bb508fd6b4ba4679e015123298264ac3"
+  sha256 "f220362622130bf6b56db86de459e696b9eeb2ba25ff721f9fd79063c2cc51f4"
 
   url "https://github.com/joewongjc/type4me/releases/download/v#{version}/Type4Me-v#{version}-cloud.dmg"
   name "Type4Me"
